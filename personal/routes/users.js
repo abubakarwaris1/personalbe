889d4dcd6994420ce6b244ee56d1efebdee9b9e7
ali hussnain <ali.husnain@dmechs.com>
2021-05-25 08:50:36 +0500

    added pingme route

--- personal/routes/users.js
+++ personal/routes/users.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/getUser", userController.user);
+router.get("/ping",(req,res)=>res.send('ok'))
 router.get("/findOneUser/:id", userController.findOneUser);
 // router.get("/getUser", ()=>{console.log('somehting................')});
 router.post("/save",userController.save);
